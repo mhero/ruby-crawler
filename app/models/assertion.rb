ebd61c0f228f36008ee0eb4f14f59ab81e87609c
Marco <gdmarav374@gmail.com>
2024-09-14 22:04:22 +0200

    Split builder into concern

--- app/models/assertion.rb
+++ app/models/assertion.rb
@@ -1,40 +1,9 @@
 class Assertion < ApplicationRecord
+  include AssertionBuilder
+
   validates :url, presence: true
   validates :text, presence: true
   validates :status, presence: true
   validates :links_number, numericality: { only_integer: true }
   validates :images_number, numericality: { only_integer: true }
-
-  def self.build_from_web_crawler(params)
-    url = params[:url]
-    return Outcome.failure("Url is not present") unless url.present?
-
-    text = params[:text]
-    return Outcome.failure("Text is not present") unless text.present?
-
-    document_outcome = WebCrawler.call(url)
-
-    if document_outcome.success?
-      analyzer = DocumentAnalyzer.call(document_outcome.value)
-
-      Outcome.success(
-        Assertion.new(
-          url: url,
-          text: text,
-          status: status(analyzer, text),
-          links_number: analyzer.count_urls,
-          images_number: analyzer.count_images
-        ),
-        "Succesful parse"
-      )
-    else
-      Outcome.failure(document_outcome.message)
-    end
-  end
-
-  private
-
-  def self.status(analyzer, text)
-    analyzer.text_exists?(text) ? "PASS" : "FAIL"
-  end
 end
