32d10c1daa8f4c400a8aaa0fc187ec096bac7932
Marco <gdmarav374@gmail.com>
2024-09-15 23:05:56 +0200

    Store local path in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_12_215307) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_15_210028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,5 +22,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_12_215307) do
     t.integer "images_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "local_path"
   end
 end
